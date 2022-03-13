f5bd7db3dadaad668082ac7d701385cb10a89b5e
Rubenennj <57841770+Rubenennj@users.noreply.github.com>
2022-03-13 02:50:25 +0100

    Reworked files

--- dist/__tests__/runner.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const child_process_1 = require("child_process");
-(0, child_process_1.fork)(`./dist/__tests__/${process.argv[2] ?? 'test'}.js`);
-//# sourceMappingURL=runner.js.map
